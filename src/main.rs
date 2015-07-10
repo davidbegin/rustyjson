ea3050c92f3f24667a7b1fb05cfd978da3235107
David Begin <davidmichaelbe@gmail.com>
2015-07-09 20:40:28 -0700

    yeah I just read json from a file no big deal

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+#![allow(unused_mut, unused_imports, unused_variables)]
+
 extern crate rustc_serialize;
+extern crate type_printer;
 use rustc_serialize::json;
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+use std::error::Error;
 
 #[derive(RustcDecodable, RustcEncodable, Debug)]
 pub struct User {
@@ -11,6 +18,31 @@ pub struct User {
 fn main() {
     println!("\nRust JSON and You");
     println!("=================\n");
+
+    let path = Path::new("src/user.json");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Ok(file) => file,
+        Err(why) => {
+            panic!(
+                "couldn't open {}: {}",
+                display, Error::description(&why)
+            )
+        }
+    };
+
+    let mut s = String::new();
+
+    match file.read_to_string(&mut s) {
+        Ok(_) => print!("{} contains:\n{}", display, s),
+        Err(why) => panic!(
+            "couldn't read {}: {}", display,
+            Error::description(&why)
+        ),
+    }
+
+    // ==========================================
 
     let user = User {
       name: "Frank Stella".to_string(),
